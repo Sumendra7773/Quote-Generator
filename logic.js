af1f8e309773117f3d1499d0e71329aa54ee9e7a
Sumendra7773 <sumendrakumar82@gmail.com>
2022-04-10 15:59:27 +0530

    new quote btn will now generate new quote '"

--- logic.js
+++ logic.js
@@ -1,5 +1,34 @@
+const quoteContainer = document.getElementById('quote-container');
+const quotText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+let quotes = [];
+
 //get Quotes from API
 async function getQuotes() {
-    const apiUrl = 'https://type.fit/api/quotes';
-    const response = await fetch(apiUrl);
+	const apiUrl = 'https://type.fit/api/quotes';
+	try {
+		const response = await fetch(apiUrl);
+		quotes = await response.json();
+		newQuote();
+	} catch (error) {
+		// catch error Here
+		console.log(error);
+	}
 }
+
+// on load
+getQuotes();
+
+// show New Quote
+function newQuote() {
+	// pick a random quote from apiQuery array
+	const quote = quotes[Math.floor(Math.random() * quotes.length)];
+	authorText.textContent = `- ${quote.author}`;
+	quotText.textContent = quote.text;
+	console.log(quotes.length);
+}
+
+newQuoteBtn.addEventListener('click', newQuote);
