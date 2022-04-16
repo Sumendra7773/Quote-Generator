dfed4d0c0ffe438420e3daeb26e1c1f028b891e2
Sumendra7773 <sumendrakumar82@gmail.com>
2022-04-16 12:48:47 +0530

    changed unknown text to Maurya jii ,Added twiter btn functionallity

--- logic.js
+++ logic.js
@@ -1,5 +1,8 @@
+// @ts-check
+
+'use strict';
 const quoteContainer = document.getElementById('quote-container');
-const quotText = document.getElementById('quote');
+const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
@@ -18,7 +21,6 @@ async function getQuotes() {
 		console.log(error);
 	}
 }
-
 // on load
 getQuotes();
 
@@ -26,9 +28,26 @@ getQuotes();
 function newQuote() {
 	// pick a random quote from apiQuery array
 	const quote = quotes[Math.floor(Math.random() * quotes.length)];
-	authorText.textContent = `- ${quote.author}`;
-	quotText.textContent = quote.text;
-	console.log(quotes.length);
+	// Check if Author field is blank and replace it with 'Maurya jii'
+	if (!quote.author) authorText.textContent = '- Maurya jii';
+	else authorText.textContent = `- ${quote.author}`;
+
+	// Check Quote length to determine styling
+	if (quote.text.Length > 50) quoteText.classList.add('long-quote');
+	else quoteText.classList.remove('long-quote');
+	// Set Quote, Hide Loader
+	quoteText.textContent = quote.text;
+	console.log(quote);
 }
 
 newQuoteBtn.addEventListener('click', newQuote);
+
+// Tweet Quote
+function tweetQuote() {
+	const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent}%20%0A%20%20%20%20%20%20%20%20%20 ${authorText.textContent}`;
+	window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
